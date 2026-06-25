67233aae0e39ab2cf78f6d868814807c866f6c07
Kshitiz Rawat <rwtchinu007@gmail.com>
2026-06-25 11:34:50 +0530

    Frontend Integration with Backend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,6 +3,7 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const morgan = require("morgan");
+const path = require("path");
 
 // require routes
 const authRouter = require("./routes/auth.route");
@@ -13,15 +14,23 @@ const userRouter = require("./routes/user.route");
 app.use(express.json());
 app.use(cookieParser());
 app.use(morgan("dev"));
-app.use(cors(
-    {
-        credentials: true,
-        origin: "http://localhost:5173", 
-        methods: ["GET", "POST", "PUT", "DELETE"],
-    }
-));
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  }),
+);
 app.use("/api/auth", authRouter);
 app.use("/api/posts", postRouter);
 app.use("/api/users", userRouter);
+
+// it is used to serve the static files from the public folder
+app.use(express.static("./public"));
+
+// it is used to serve the index.html file for any route that does not match the above routes
+app.use("*name", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "/public/index.html"));
+});
 
 module.exports = app;
--- Frontend/src/features/profile/pages/Profile.jsx
+++ Frontend/src/features/profile/pages/Profile.jsx
@@ -145,4 +145,4 @@ const Profile = () => {
   );
 };
 
-export default Profile;
+export default Profile; 
